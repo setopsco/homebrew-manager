2d7f0b4d63d5c10a5b15417c3c76b70bb7dbc016
Jonas Hochstrat <jhoch@jhoch.de>
2020-11-09 15:02:53 +0100

    :recycle: Corrects formula style

--- Formula/manager.rb
+++ Formula/manager.rb
@@ -1,19 +1,19 @@
 class Manager < Formula
-  desc "SetOps infrastructure manager – CLI & REST API for developers to manage stages, apps and services"
-  homepage "https://zweitag.de"
-  url "https://cefca4284b498ce2b82d43703c87fa81d1bcc3fe@api.github.com/repos/setopsco/manager/releases/assets/27964702",
-  header: 'Accept: application/octet-stream' 
-  version "0.0.5"
-  sha256 "ac2435bb4c8ff1effe93a8348b55403596542216dc7f73c6808ce8974f190f84"
+  desc 'SetOps infrastructure manager – CLI & REST API for developers to manage stages, apps and services'
+  homepage 'https://zweitag.de'
+  url 'https://cefca4284b498ce2b82d43703c87fa81d1bcc3fe@api.github.com/repos/setopsco/manager/releases/assets/27964702',
+      header: 'Accept: application/octet-stream'
+  version '0.0.5'
+  sha256 'ac2435bb4c8ff1effe93a8348b55403596542216dc7f73c6808ce8974f190f84'
   # TODO: Update license (see https://docs.brew.sh/Formula-Cookbook)
-  license "MIT"
+  license 'MIT'
 
   bottle :unneeded
 
   # TODO: Update name of the formula to decrease the chance of clonflicts
 
-  GITHUB_HOMEBREW_TOKEN = 'cefca4284b498ce2b82d43703c87fa81d1bcc3fe'
-  RELEASE_FILE_NAME = 'setops-cli_v0.0.5_darwin_amd64'
+  GITHUB_HOMEBREW_TOKEN = 'cefca4284b498ce2b82d43703c87fa81d1bcc3fe'.freeze
+  RELEASE_FILE_NAME = 'setops-cli_v0.0.5_darwin_amd64'.freeze
 
   def install
     bin.install RELEASE_FILE_NAME => 'setops'
@@ -25,7 +25,7 @@ class Manager < Formula
     # install the completion scripts
     system "#{prefix}/bin/setops"
     zsh_completions_temp = Tempfile.new('setops-manager-zsh-completions')
-    zsh_completions_temp.write(Utils.popen_read("#{prefix}/bin/setops" ,"completion","zsh"))
+    zsh_completions_temp.write(Utils.popen_read("#{prefix}/bin/setops", 'completion', 'zsh'))
     zsh_completions_temp.rewind
 
     zsh_completion.install zsh_completions_temp.path => '_setops'
@@ -34,10 +34,10 @@ class Manager < Formula
     zsh_completions_temp.unlink
 
     bash_completions_temp = Tempfile.new('setops-manager-zsh-completions')
-    bash_completions_temp.write(Utils.popen_read("#{prefix}/bin/setops" ,"completion","bash"))
+    bash_completions_temp.write(Utils.popen_read("#{prefix}/bin/setops", 'completion', 'bash'))
     bash_completions_temp.rewind
 
-    bash_completion.install bash_completions_temp.path => "setops"
+    bash_completion.install bash_completions_temp.path => 'setops'
 
     bash_completions_temp.close
     bash_completions_temp.unlink
@@ -46,6 +46,6 @@ class Manager < Formula
   end
 
   test do
-    system "setops"
+    system 'setops'
   end
 end
