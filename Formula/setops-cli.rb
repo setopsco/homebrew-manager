dab4fcabf398f25b00b609fae2015b3922974b2c
SetOps GitHub Action <admin@setops.co>
2021-09-28 21:32:53 +0000

    :arrow_up: Updates formula to v0.7.3

--- Formula/setops-cli.rb
+++ Formula/setops-cli.rb
@@ -1,16 +1,16 @@
 class SetopsCli < Formula
   desc "The only tool you need to run your cloud applications"
   homepage "https://setops.co"
-  url "https://api.github.com/repos/setopsco/releases/releases/assets/45767039",
+  url "https://api.github.com/repos/setopsco/releases/releases/assets/45795057",
       header: "Accept: application/octet-stream"
-  version "0.7.2"
-  sha256 "11a4ea2ac4082bcd627fb846e630141a91246dd19308664aea027db7ec8764cc"
+  version "0.7.3"
+  sha256 "3bd843c81d01da577c59fa9e95e899792e99f8f4659335c5342e23e033d24974"
   # TODO: Update license (see https://docs.brew.sh/Formula-Cookbook)
   license :cannot_represent
 
   bottle :unneeded
 
-  RELEASE_FILE_NAME = "setops-cli_v0.7.2_darwin_amd64".freeze
+  RELEASE_FILE_NAME = "setops-cli_v0.7.3_darwin_amd64".freeze
 
   def install
     bin.install RELEASE_FILE_NAME => "setops"
