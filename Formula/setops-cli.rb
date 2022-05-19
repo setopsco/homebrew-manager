548106949083ab4ad3788fffc7bcad8f4b71bcc1
Jonas Hochstrat <jhoch@jhoch.de>
2022-05-19 17:26:14 +0200

    :bug: Reactivate completion scripts

--- Formula/setops-cli.rb
+++ Formula/setops-cli.rb
@@ -18,26 +18,24 @@ class SetopsCli < Formula
     system "chmod 555 #{bin}/setops"
 
     # install the completion scripts
-    # -> deactivated until completions are fixed in v1.0.1
-    #
-    # system "#{bin}/setops"
-    # zsh_completions_temp = Tempfile.new("setops-manager-zsh-completions")
-    # zsh_completions_temp.write(Utils.safe_popen_read("#{bin}/setops", "completion", "zsh"))
-    # zsh_completions_temp.rewind
+    system "#{bin}/setops"
+    zsh_completions_temp = Tempfile.new("setops-manager-zsh-completions")
+    zsh_completions_temp.write(Utils.safe_popen_read("#{bin}/setops", "completion", "zsh"))
+    zsh_completions_temp.rewind
 
-    # zsh_completion.install zsh_completions_temp.path => "_setops"
+    zsh_completion.install zsh_completions_temp.path => "_setops"
 
-    # zsh_completions_temp.close
-    # zsh_completions_temp.unlink
+    zsh_completions_temp.close
+    zsh_completions_temp.unlink
 
-    # bash_completions_temp = Tempfile.new("setops-manager-zsh-completions")
-    # bash_completions_temp.write(Utils.safe_popen_read("#{bin}/setops", "completion", "bash"))
-    # bash_completions_temp.rewind
+    bash_completions_temp = Tempfile.new("setops-manager-zsh-completions")
+    bash_completions_temp.write(Utils.safe_popen_read("#{bin}/setops", "completion", "bash"))
+    bash_completions_temp.rewind
 
-    # bash_completion.install bash_completions_temp.path => "setops"
+    bash_completion.install bash_completions_temp.path => "setops"
 
-    # bash_completions_temp.close
-    # bash_completions_temp.unlink
+    bash_completions_temp.close
+    bash_completions_temp.unlink
   end
 
   test do
